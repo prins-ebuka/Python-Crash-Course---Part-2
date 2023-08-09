120b15b9edee4167fbb1d24cd8910d7b5d021fb8
prins-ebuka <ecihekwereme@gmail.com>
2023-08-09 23:05:51 +0100

    Python Crash Course - PART 2

---
+++ alien_invasion.py
@@ -0,0 +1,38 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behaviour."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color.
+        self.bg_color 
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
+
+#my notes
+# first we imported sys and pygame modules. The Pygame module contains the functionally to make a game.
+# we use the sys module to exit the game when the player quits.
+
+
