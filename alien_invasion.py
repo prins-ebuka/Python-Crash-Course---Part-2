16dee073a23027ebff5ec179f3113b8c138c8174
prins-ebuka <ecihekwereme@gmail.com>
2023-08-13 20:42:34 +0100

    --h

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,17 +23,25 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
+            # to call a method within a class we use the dot notation with the variable self and the name of the method.
+            self._check_events()
+            self._update_screen()
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color) 
-            self.ship.blitme()
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
@@ -46,4 +54,11 @@ if __name__ == '__main__':
 # at some point, we create a class Settings to hold the settings of the game and imported it 
 # into the game (alien_invasion) class.
 
+# Since it has been long and this is the first time doing this project, I need to revise all I have done so far.
+# 3 things that look quite strange are the way we first initialised the pygame.init() and
+# modules like display etc..
 
+# Page 236, I am in "Refactoring the _check events() and _update_screen() Methods."
+# In this section, we'll break the run-game() method, which is getting lengthy, into two "helper methods."
+# A "helper method" does work inside a class but isn't meant to be called through an instance. 
+# In Python, a single leading undescore indicates a helper nethod.
