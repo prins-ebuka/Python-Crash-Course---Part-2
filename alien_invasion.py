98127119e6bd206149d39268abd9872c2c9fb1eb
prins-ebuka <ecihekwereme@gmail.com>
2023-08-13 06:13:07 +0100

    --h

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,18 +2,25 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
 
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship(self)
 
         # Set the background color.
-        self.bg_color 
+        self.bg_color = (230,   230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -22,6 +29,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color) 
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
@@ -34,5 +45,8 @@ if __name__ == '__main__':
 #my notes
 # first we imported sys and pygame modules. The Pygame module contains the functionally to make a game.
 # we use the sys module to exit the game when the player quits.
+# at some point, we create a class Settings to hold the settings of the game and imported it 
+# into the game (alien_invasion) class.
 
+## I cannot seem to view the ship in my game... what might be wrong.
 
---
+++ settings.py
@@ -0,0 +1,11 @@
+# Instead of doing all our setting in the game file which is the alien_invasion file
+# we will save the settings here in this module.
+
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
