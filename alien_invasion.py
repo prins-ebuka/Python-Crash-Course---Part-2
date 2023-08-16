db0b07d95c1902a479d5426b073bc4f4254c6036
prins-ebuka <ecihekwereme@gmail.com>
2023-08-16 17:02:26 +0100

    --h

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,6 +13,11 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
+        #Running the game in fullscreen mode. Since I do not enjoy the fullscreen I will comment this section
+        #self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        #self.settings.screen_width = self.screen.get_rect().width
+        #self.settings.screen_height = self.screen.get_rect().height
+
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
@@ -25,6 +30,7 @@ class AlienInvasion:
         while True:
             # to call a method within a class we use the dot notation with the variable self and the name of the method.
             self._check_events()
+            self.ship.update()
             self._update_screen()
             # Watch for keyboard and mouse events.
             
@@ -33,6 +39,39 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            # My comment: Please refer to the Piloting Ship section below.
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+                if event.type == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+                    # Move the ship to the right.
+                    self.ship.rect.x +=1
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        # Pressing Q to Quit
+        elif event.key == pygame.K_q:
+            sys.exit
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
@@ -62,3 +101,10 @@ if __name__ == '__main__':
 # In this section, we'll break the run-game() method, which is getting lengthy, into two "helper methods."
 # A "helper method" does work inside a class but isn't meant to be called through an instance. 
 # In Python, a single leading undescore indicates a helper nethod.
+
+# Piloting the Ship
+# Responding to a Keypress: Whenever the player presses a key, that key-press is registered in Pygame as an event.
+# Each event is picked up by the pygame.event.get() method.
+# Each keypress is registered as a KEYDOWN event.
+
+# With all the complications I am seeing here, I need to comeback to this to try and capture the main points.
--- settings.py
+++ settings.py
@@ -9,3 +9,6 @@ class Settings:
         self.screen_width = 800
         self.screen_height = 400
         self.bg_color = (230, 230, 230)
+
+        # Ship settings - We are simply adjusting the ship's speed.
+        self.ship_speed = 1.5
